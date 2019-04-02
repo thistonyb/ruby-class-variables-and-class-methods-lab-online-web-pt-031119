1f1e1779665454f29b10964e934820da895b9ea6
Tony Baker <this.tonybaker@gmail.com>
2019-04-02 18:17:40 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+  @@count = 0
+  @@genres = []
+  attr_accessor :name, :artist, :genre
+
+  def initialize(song_name, song_artist, song_genre)
+    @name = song_name
+    @artist = song_artist
+    @genre = song_genre
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres
+  end
+
+  
+end
